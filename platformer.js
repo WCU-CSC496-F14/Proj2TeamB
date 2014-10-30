fe5178aaef9c009a464f1a5a37596168d8199488
CM137 <ChrisMason137@gmail.com>
2014-10-30 01:57:17 -0400

    Title Screen
    Title Screen and changed colors of win/lose game alerts

--- platformer.js
+++ platformer.js
@@ -134,10 +134,31 @@ Q.scene('endGame',function(stage) {
     x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
   }));
 
-  var button = container.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC",
+  var button = container.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#FFFFFF",
                                                   label: "Play Again" }))         
-  var label = container.insert(new Q.UI.Text({x:10, y: -10 - button.p.h, 
-                                                   label: stage.options.label }));
+  var label = container.insert(new Q.UI.Text({x:10, y: -20 - button.p.h, 
+                                                   label: stage.options.label, color: "white" }));
+  // When the button is clicked, clear all the stages
+  // and restart the game.
+  button.on("click",function() {
+    Q.clearStages();
+    Q.stageScene('level1');
+  });
+
+  // Expand the container to visibily fit it's contents
+  // (with a padding of 20 pixels)
+  container.fit(20);
+});
+
+Q.scene('title',function(stage) {
+  var container = stage.insert(new Q.UI.Container({
+    x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
+  }));
+
+  var button = container.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#FFFFFF",
+                                                  label: "Play" }))         
+  var label = container.insert(new Q.UI.Text({x:10, y: -20 - button.p.h, 
+                                                   label: stage.options.label, color: "white" }));
   // When the button is clicked, clear all the stages
   // and restart the game.
   button.on("click",function() {
@@ -162,7 +183,7 @@ Q.load("sprites.png, sprites.json, level.json, tiles.png, background-wall.png", 
   Q.compileSheets("sprites.png","sprites.json");
 
   // Finally, call stageScene to run the game
-  Q.stageScene("level1");
+  Q.stageScene("title",1, { label: "Super Awesome Platformer" }); 
 });
 
 // ## Possible Experimentations:
