9d85ee5b361bb567095c686d5ce0bc4b8a50503d
David-S-Hunsicker <david.s.hunsicker@gmail.com>
2014-11-07 02:05:42 -0500

    fixed auto writes that git-hub made at revert

--- platformer.js
+++ platformer.js
@@ -12,18 +12,17 @@ window.addEventListener("load",function() {
 // Set up an instance of the Quintus engine  and include
 // the Sprites, Scenes, Input and 2D module. The 2D module
 // includes the `TileLayer` class as well as the `2d` componet.
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 var Q = window.Q = Quintus({audioSupported: ['mp3']})
         .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI", "Audio")
-=======
+
 var Q = window.Q = Quintus()
         .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")
->>>>>>> parent of a425d16... rick-astley
-=======
+
+
 var Q = window.Q = Quintus()
         .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")
->>>>>>> parent of a425d16... rick-astley
+
         // Maximize this game to whatever the size of the browser is
         .setup({ maximize: true })
         // And turn on default input controls and touch input (for UI)
