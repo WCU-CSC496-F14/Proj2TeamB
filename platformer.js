da3cb673964468458fca5126fb90fc5f2b8e58b4
David-S-Hunsicker <david.s.hunsicker@gmail.com>
2014-11-07 01:29:11 -0500

    2 p's in supported, not 3

--- platformer.js
+++ platformer.js
@@ -12,7 +12,7 @@ window.addEventListener("load",function() {
 // Set up an instance of the Quintus engine  and include
 // the Sprites, Scenes, Input and 2D module. The 2D module
 // includes the `TileLayer` class as well as the `2d` componet.
-var Q = window.Q = Quintus({audioSuppported: ['mp3']})
+var Q = window.Q = Quintus({audioSupported: ['mp3']})
         .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI", "Audio")
         // Maximize this game to whatever the size of the browser is
         .setup({ maximize: true })
