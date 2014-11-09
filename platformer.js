110da1c35dd5f97658271403cc5af233e14a464d
CM137 <ChrisMason137@gmail.com>
2014-11-09 18:07:35 -0500

    Changes
    Changes

--- platformer.js
+++ platformer.js
@@ -95,7 +95,7 @@ Q.Sprite.extend("Player",{
           }
           this.p.direction = "left";
         } else {
-          this.play("stand_right");
+          this.play("stand_" + this.p.direction);
         }
     }
   }
