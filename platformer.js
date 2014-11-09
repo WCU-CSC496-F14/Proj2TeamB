63e167b596471266e5ed8aac6ef5417a2ada20c9
CM137 <ChrisMason137@gmail.com>
2014-11-09 18:30:36 -0500

    Change
    Change

--- platformer.js
+++ platformer.js
@@ -239,7 +239,7 @@ Q.load("spritesheet.png, sprites.json, level.json, newtiles.png, cavebackground.
 //Q.audio.play("Rick-astley.mp3",{ loop: true });
   // Or from a .json asset that defines sprite locations
   Q.compileSheets("spritesheet.png","sprites.json");
-  Q.animations("Player", {
+  Q.animations('player', {
       walk_right: { frames: [0,1,2,3,4,5,6,7], rate: 1/15, flip: false, loop: true },
       walk_left: { frames:  [8,9,10,11,12,13,14,15], rate: 1/15, flip: false, loop: true },
       jump_right: { frames: [16], rate: 1/10, flip: false },
