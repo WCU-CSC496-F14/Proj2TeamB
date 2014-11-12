950b0a3ca4820e0a3912aa70abe616536ca3a6b1
CM137 <ChrisMason137@gmail.com>
2014-11-11 19:34:09 -0500

    Different enemy
    yeah

--- platformer.js
+++ platformer.js
@@ -212,8 +212,8 @@ Q.scene("level2",function(stage) {
   stage.insert(new Q.Enemy({ x: 800, y: 1000 }));
   stage.insert(new Q.Enemy({ x: 600, y: 800 }));
   stage.insert(new Q.Enemy({ x: 1100, y: 800 }));
-  stage.insert(new Q.Enemy({ x: 1500, y: 620 }));
-  //stage.insert(new Q.Enemy({ x: 1400, y: 620 }));
+  //stage.insert(new Q.Enemy({ x: 1500, y: 620 }));
+  stage.insert(new Q.Enemy({ x: 1400, y: 620 }));
   stage.insert(new Q.Enemy({ x: 1700, y: 500 }));
   stage.insert(new Q.Enemy({ x: 800, y: 200 }));
   stage.insert(new Q.Enemy({ x: 900, y: 100 }));
